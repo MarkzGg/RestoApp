b92b41e5a7970d25c9c99f473969a2e55b6fcd4f
MarkzGg <private.m2.martos@gmail.com>
2024-06-25 03:28:39 -0500

    trytoconnecttogitpages

--- restoweb/src/components/home/modal.vue
+++ restoweb/src/components/home/modal.vue
@@ -1,20 +1,24 @@
 <template>
     <div class="modal">
+        <h1>{{ msg }}</h1>
       <div class="modal-content">
         <span class="close" @click="$emit('close')">&times;</span>
         <p>Detalles de la orden {{ orderIndex + 1 }}</p>
-        Aquí se pueden agregar más detalles de la orden
+        <!-- Aquí se pueden agregar más detalles de la orden -->
       </div>
     </div>
   </template>
   
   <script>
   export default {
-    props: ['orderIndex'],
-  };
-  </script> -->
+  name: 'modalWeb',
+  props: {
+    msg: String
+  }
+}
+  </script>
   
-  <style scoped> -->
+  <style scoped>
   /* Añade aquí los estilos específicos para el modal de órdenes */
-  <!-- </style> -->
+  </style>
   
---
+++ restoweb/src/routes.js
@@ -0,0 +1,16 @@
+//import Home from '../src/views/home.view.vue'
+//import User from '../src/views/user.view.vue'
+//import Inventary from '../src/views/inventary.view.vue'
+//import Orders from '../src/views/orders.view.vue'
+//import More from '../src/views/more.view.vue'
+/* import HelloWord from '../src/components/HelloWorld'
+
+
+
+
+//const Home = {template:'<h1>Hola home</h1>'}
+const User = {template:'<h1>Hola User</h1>'}
+const Inventary = {template:'<h1>Hola Inventario</h1>'}
+const Orders = {template:'<h1>ordeness</h1>'}
+const More = {template:'<h1>more</h1>'}
+export default routes; */
